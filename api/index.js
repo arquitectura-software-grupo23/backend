d14c224723cfc2a8713bf0346cd96358fa831d87
Vicente Del Fierro <vicente.delfierro@uc.cl>
2023-11-28 22:27:42 -0300

    welive

--- api/index.js
+++ api/index.js
@@ -169,7 +169,7 @@ const createTransbankTransaction = async (price) => {
     'ID',
     'ID',
     Math.ceil(price * 1000),
-    'http://localhost:5173/validate',
+    'http://frontend.stocknet.me/validate',
   );
 
   return transaction;
